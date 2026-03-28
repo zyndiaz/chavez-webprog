d5b878b000b9a3d2c4d60c8bdc968a6c3abe0501
Franzyn Chavez <franzyn14@gmail.com>
2026-03-28 19:34:04 +0800

    lab-act1

---
+++ chavez-client/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import React from 'react';
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Welcome to My React App!</h1>
+        <p>
+          Name: Franzyn D. Chavez<br />
+          Section: INF231<br />
+          Year Level: 3rd<br />
+          Course: BS Information Technology<br />
+          Specialization: Mobile and Web Application<br />
+          School: National University - Manila<br />
+          Email: chavezfd@students.national-u.edu.ph<br />
+          Age: 21<br />
+          Birthdate: August 14, 2004<br />
+          Birth Place: Lucena City<br />
+          Hometown: Lucban, Quezon<br />
+          Residence: Taytay, Rizal
+        </p>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
