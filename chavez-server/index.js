b3f8a5bec34841ce86bb84fbdbb657db939ea4db
zyndiaz <franzyn14@gmail.com>
2026-06-02 21:31:22 +0800

    lab-act7

--- chavez-server/index.js
+++ chavez-server/index.js
@@ -3,39 +3,19 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
-//const articleRoutes = require("./routes/articleRoutes");
 
 const app = express();
 
 connectDB();
 
-const allowedOrigins = [
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "https://chavez-client.vercel.app"
-];
-
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            console.log("Blocked origin:", origin);
-            callback(null, true); 
-        }
-    },
+    origin: ["http://localhost:5173", "https://chavez-client.vercel.app"],
     credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    optionsSuccessStatus: 204,
 };
 app.use(cors(corsOptions));
-
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
-app.use("/api/articles", articleRoutes);
 
 app.get("/", (req, res) => {
     res.json({ message: "Chavez Server is running!" });
