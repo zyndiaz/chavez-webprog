47dca09a709f29261e61f2757262e26e29aaff65
zyndiaz <franzyn14@gmail.com>
2026-06-02 21:20:47 +0800

    ;ab-act7

--- chavez-server/index.js
+++ chavez-server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
-const articleRoutes = require("./routes/articleRoutes");
+//const articleRoutes = require("./routes/articleRoutes");
 
 const app = express();
 
